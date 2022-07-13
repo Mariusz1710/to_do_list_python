e8af946709166023785e01fae2c5dba839260fb2
Mariusz Bielicki <mbielicki@wp.pl>
2022-07-13 23:52:15 +0200

    Add task with status

--- main.py
+++ main.py
@@ -1,22 +1,49 @@
 import os
-
+from colorama import Fore,Back,Style
 
 tasks = {"wash the dishes":"to do","give Misiowa her food":"to do","go shopping":"done","learn programming":"in progress"}
 
 def show_tasks():
     i = 1
     print("-------------------------------------------------------------------")
-    for task in tasks:
-        print(str(i) + ". " + task)
+    for task,status in tasks.items():
+        if status == "to do":
+            print(Fore.RED + str(i) + ". " + task)
+        elif status == "done":
+            print(Fore.GREEN + str(i) + ". " + task)
+        elif status == "in progress":
+            print(Fore.BLUE + str(i) + ". " + task)
         i += 1
+        print(Style.RESET_ALL)
     print("-------------------------------------------------------------------")
     os.system("pause")
 
 def add_task():
-    task = input("Enter a task:")
-    tasks.append(task)
-    print("The task has been added")
-    os.system("pause")
+    choice = "a"
+    status = -1
+    while choice != "x":
+        print("Enter 'x' as a task if you want to exit")
+        task = input("Enter a task:")
+        if task == "x":
+            return
+        while True:
+            status = int(input("""Enter the status of the task:
+1. To do
+2. In progress
+3. Done"""))
+            if status in (1,2,3):
+                if status == 1:
+                    tasks[task] = "to do"
+                elif status == 2:
+                    tasks[task] = "in progress"
+                elif status == 3:
+                    tasks[task] = "done"
+                print(tasks)
+                print("The task has been added")
+                os.system("pause")
+                return
+            else:
+                print("The wrong status!")
 
 def del_task():
     task = int(input("Enter the number of the task which you want to delete"))
